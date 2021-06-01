a277d5a02c35b6b51ac34761d4de96d4e2e36c9e
Bobby Towers <porkostomus@mail.com>
2021-05-31 20:34:32 -0600

    Create babashka_test_runner.clj

---
+++ src/babashka_test_runner.clj
@@ -0,0 +1,15 @@
+
+(ns babashka-test-runner
+  (:require
+   [clojure.test :refer [deftest testing is]]
+   [cheshire.core :as json])
+  (:gen-class))
+
+(defn -main
+  "Tests a solution file and writes `results.json` to output directory"
+  [slug input output]
+  (spit (str output "results.json")
+        (json/generate-string {:slug slug :input input :output output})))
+
+(comment
+  (-main "two-fer" "in-dir/" "out-dir/"))
